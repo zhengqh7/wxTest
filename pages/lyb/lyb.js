fec5214cdc0439d03b292760aa5bf52ced176202
xiajianhua <1464506594@qq.com>
2019-03-27 10:07:42 +0800

    hhhh

--- pages/lyb/lyb.js
+++ pages/lyb/lyb.js
@@ -1,184 +1,165 @@
-const app = getApp();
 Page({
-  /**
-   * 页面的初始数据
-   */
   data: {
-    msgData:
+    array: ['美国', '中国', '巴西', '日本'],
+    objectArray: [
+      {
+        id: 0,
+        name: '美国'
+      },
+      {
+        id: 1,
+        name: '中国'
+      },
+      {
+        id: 2,
+        name: '巴西'
+      },
+      {
+        id: 3,
+        name: '日本'
+      }
+    ],
+    index: 0,
+    multiArray: [['无脊柱动物', '脊柱动物'], ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物'], ['猪肉绦虫', '吸血虫']],
+    objectMultiArray: [
       [
         {
-          child_id: 1,
-          msg:"我想做个眉毛，到店后求推荐。",
-          checked: ''
+          id: 0,
+          name: '无脊柱动物'
         },
         {
-          child_id: 2,
-          msg:"我只有2小时，您看着安排吧。",
-          checked: ''
-        },
-        {
-          child_id: 3,
-          msg:"我和朋友一起过来。",
-          checked: ''
-        },
-        {
-          child_id: 4,
-          msg:"美甲样式到店挑选。",
-          checked: ''
-        },
-        {
-          child_id: 5,
-          msg:"给眉毛再补个颜色。",
-          checked: ''
-        },
-        {
-          child_id: 6,
-          msg:"我要补睫毛哦。",
-          checked: ''
+          id: 1,
+          name: '脊柱动物'
         }
-      ],
-    message: '',
-    message_id: [],
+      ], [
+        {
+          id: 0,
+          name: '扁性动物'
+        },
+        {
+          id: 1,
+          name: '线形动物'
+        },
+        {
+          id: 2,
+          name: '环节动物'
+        },
+        {
+          id: 3,
+          name: '软体动物'
+        },
+        {
+          id: 3,
+          name: '节肢动物'
+        }
+      ], [
+        {
+          id: 0,
+          name: '猪肉绦虫'
+        },
+        {
+          id: 1,
+          name: '吸血虫'
+        }
+      ]
+    ],
+    multiIndex: [0, 0, 0],
+    date: '2016-09-01',
+    time: '12:01',
+    region: ['广东省', '广州市', '海珠区'],
+    customItem: '全部'
   },
-  bindTextAreaChange: function (e) {
-    var that = this
-    that.setData({
-      message: e.detail.value
-    })
-  },
-  click: function (e) {
-    var that = this;
-    let id = e.currentTarget.dataset.id;
-    let index = e.currentTarget.dataset.index;
-    var value = [];
-    value = this.data.message_id;
-    var array_i = this.in_array(id, value);
-    var chekeds = that.data.msgData;
-    var msg = chekeds[index].msg;
-    var message = that.data.message;
-    if (!e.currentTarget.dataset.checked) {
-      chekeds[index].checked = true
-      that.setData({
-        message: message + msg
-      })
-    } else {
-      chekeds[index].checked = false
-      that.setData({
-        message: message.replace(msg, '')
-      })
-    }
-    that.setData({
-      msgData: chekeds
-    })
-    if (array_i) {
-      value.splice(array_i, 1);
-    } else {
-      value.push(id);
-    }
+  bindPickerChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
     this.setData({
-      message_id: value,
+      index: e.detail.value
     })
   },
-  in_array: function (search, array) {
-    for (var i in array) {
-      if (array[i] == search) {
-        return i;
-      }
-    }
-    return false;
+  bindMultiPickerChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      multiIndex: e.detail.value
+    })
   },
-  submit: function () {
-    var value = [];
-    var message = this.data.message;
-    var msgData = this.data.msgData;
-    if (message == '' && !value.length) {
-      wx.showToast({
-        title: '暂无选择项目',
-        icon: 'none'
-      })
-      return;
+  bindMultiPickerColumnChange(e) {
+    console.log('修改的列为', e.detail.column, '，值为', e.detail.value)
+    const data = {
+      multiArray: this.data.multiArray,
+      multiIndex: this.data.multiIndex
     }
-    app.globalData.message = message;
-    for (var i = 0; i < msgData.length; i++) {
-      if (message.indexOf(msgData[i].msg) > -1) {
-        value[i] = msgData[i].child_id;
-      }
-    }
-    wx.request({
-      url: 'http://132.232.88.15/wxv2/lyb.ashx',
-      method: 'POST',
-      data: {message_id: value, openId: app.globalData.openId, message: message, token: app.globalData.token},
-      header:{'Accept': 'application/json'},
-      success: function (res) {
-        if (res) {
-          wx.showToast({
-             title: '捎话成功',
-            icon: 'none'
-           })
-          wx.switchTab({
-            url: '/pages/orders/orders',
-            success: function (e) {
-              var page = getCurrentPages().pop();
-              if (page == undefined || page == null) return;
-              page.onLoad();
-            }
-          })
-          app.globalData.message = message;
+    data.multiIndex[e.detail.column] = e.detail.value
+    switch (e.detail.column) {
+      case 0:
+        switch (data.multiIndex[0]) {
+          case 0:
+            data.multiArray[1] = ['扁性动物', '线形动物', '环节动物', '软体动物', '节肢动物']
+            data.multiArray[2] = ['猪肉绦虫', '吸血虫']
+            break
+          case 1:
+            data.multiArray[1] = ['鱼', '两栖动物', '爬行动物']
+            data.multiArray[2] = ['鲫鱼', '带鱼']
+            break
         }
-        console.log(res)
-      }
-    })
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    var that = this;
-    wx.request({
-      url: 'http://132.232.88.15/wxv2/lyb.ashx',
-      method: 'POST',
-      data: {openId:app.globalData.openId,token:app.globalData.token},
-      header:{'Accept':'application/json'},
-      success:function (res) {
-        if (res.data) {
-          var message_id = res.data.message_id;
-          var value = that.data.msgData;
-          var message = res.data.message;
-          that.setData({
-            message: message
-          });
-          for (var i = 0; i < value.length; i++) {
-            if (that.in_array(value[i].child_id, message_id)) {
-              value[i].checked = true;
-              that.setData({
-                msgData: value,
-              });
+        data.multiIndex[1] = 0
+        data.multiIndex[2] = 0
+        break
+      case 1:
+        switch (data.multiIndex[0]) {
+          case 0:
+            switch (data.multiIndex[1]) {
+              case 0:
+                data.multiArray[2] = ['猪肉绦虫', '吸血虫']
+                break
+              case 1:
+                data.multiArray[2] = ['蛔虫']
+                break
+              case 2:
+                data.multiArray[2] = ['蚂蚁', '蚂蟥']
+                break
+              case 3:
+                data.multiArray[2] = ['河蚌', '蜗牛', '蛞蝓']
+                break
+              case 4:
+                data.multiArray[2] = ['昆虫', '甲壳动物', '蛛形动物', '多足动物']
+                break
             }
-          }
+            break
+          case 1:
+            switch (data.multiIndex[1]) {
+              case 0:
+                data.multiArray[2] = ['鲫鱼', '带鱼']
+                break
+              case 1:
+                data.multiArray[2] = ['青蛙', '娃娃鱼']
+                break
+              case 2:
+                data.multiArray[2] = ['蜥蜴', '龟', '壁虎']
+                break
+            }
+            break
         }
-      }
+        data.multiIndex[2] = 0
+        break
+    }
+    console.log(data.multiIndex)
+    this.setData(data)
+  },
+  bindDateChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      date: e.detail.value
     })
   },
-
-  /**   * 生命周期函数--监听页面初次渲染完成   */
-  onReady: function () {
+  bindTimeChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      time: e.detail.value
+    })
   },
-  /**   * 生命周期函数--监听页面显示   */
-  onShow: function () {
-  },
-  /**   * 生命周期函数--监听页面隐藏   */
-  onHide: function () {
-  },
-  /**   * 生命周期函数--监听页面卸载   */
-  onUnload: function () {
-  },
-  /**   * 页面相关事件处理函数--监听用户下拉动作   */
-  onPullDownRefresh: function () {
-  },
-  /**   * 页面上拉触底事件的处理函数   */
-  onReachBottom: function () {
-  },
-  /** * 用户点击右上角分享  */
-  onShareAppMessage: function () {
+  bindRegionChange(e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      region: e.detail.value
+    })
   }
 })
