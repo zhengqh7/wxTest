6a8dbe1c855ae6da4d140c1da6119decd4c2c566
fjyazzy <2881519@qq.com>
2019-04-01 13:01:10 +0800

    new change Lilklist

--- components/Linklist/Linklist.js
+++ components/Linklist/Linklist.js
@@ -1,6 +1,23 @@
 // Linklist/Linklist.js
 Component({
   data: {
+    ItemList: [{
+      pagePath: "/pages/Wxmls/wxml",
+      text: "学习WXML第一部分"
+    }, {
+      pagePath: "/pages/Wxmls2/wxml2",
+      text: "学习WXML第二部分"
+    }, {
+      pagePath: "/pages/lists/lists",
+      text: "列表"
+    }, {
+      pagePath: "/pages/orders/orders",
+      text: "服务购买"
+    }, {
+      pagePath: "/pages/table/table",
+      text: "环境配置"
+    }
+    ],
   },
   attached() {
   },
