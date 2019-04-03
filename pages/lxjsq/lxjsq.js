3af64e505e53dfdc1078a5cd667059d3a8add5c0
xiajianhua <1464506594@qq.com>
2019-04-03 15:19:52 +0800

    1231231231

--- pages/lxjsq/lxjsq.js
+++ pages/lxjsq/lxjsq.js
@@ -1,66 +1,41 @@
 // pages/lxjsq/lxjsq.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    benjin: '',
+    lixi: '',
+    mon: '',
+    still: [],
+    still1: [],
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  userBenjinInput: function (e) {
+    this.setData({
+      benjin: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  userLixiInput: function (e) {
+    this.setData({
+      lixi: e.detail.value
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  userMon: function (e) {
+    this.setData({
+      mon: e.detail.value,
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  jisuan: function (e) {
+    var benjin = this.data.benjin;
+    var lixi = this.data.lixi;
+    var mon = this.data.mon;
+    var still1 = new Array();
+    var still = new Array();
+    var still = this.data.still;
+    for (var i = 0; i < mon; i++) {
+      benjin = (benjin * (lixi / 100)) + benjin * 1;
+      still1[i] = benjin;
+    }
+    this.setData({
+      still: still1
+    })
   }
 })
